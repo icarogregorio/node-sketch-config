183d438fa4c397c64daca836df0c108349717ed0
icarogregorio <icarogregorio@hotmail.com>
2018-07-05 23:47:46 -0300

    correction

--- sketch-config.js
+++ sketch-config.js
@@ -1,29 +1,64 @@
+
 module.exports = function(RED) 
 {
-    /*function sketchConfigNode(config) {
+    //########################## NODE CONFIG ##########################//
+    function nodeConfigx(n) 
+    {
+        RED.nodes.createNode(this,n);
+        this.host = n.host;
+        this.port = n.port;
+    }
+    RED.nodes.registerType("configx",nodeConfigx);
+
+    function nodeSketch(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+        this.server = RED.nodes.getNode(config.server);
         node.on('input', function(msg) {
             msg.payload = msg.payload.toLowerCase();
+            msg.config = {
+                "host": this.server.host,
+                "port": this.server.port
+            };
             node.send(msg);
         });
     }
-    RED.nodes.registerType("node-sketch-config",sketchConfigNode);*/
-
-    function nodeSketchConfig(config) {
+    RED.nodes.registerType("node-sketch-config",nodeSketch);
+    
+};
+/*
+//########################## DEFAULT NODE ##########################//
+module.exports = function(RED)
+{
+    function nodeSketchConfig(config) 
+    {
         RED.nodes.createNode(this,config);
         
-
         // Retrieve the config node
-        this.server = RED.nodes.getNode(config.server);
+        //this.server = RED.nodes.getNode(configx.server);
 
-        if (this.server) {
+        if (this.server) 
+        {
             // Do something with:
             //  this.server.host
             //  this.server.port
-        } else {
+        } 
+        else 
+        {
             // No config node configured
         }
+
+        //Retrive input and send output with lowercase
+        this.on('input', function(msg)
+        {
+            msg.payload = msg.payload.toLowerCase();
+            msg.config = {
+                "host": server.host,
+                "port": server.port
+            }
+            node.send(msg);
+        });
+        RED.nodes.registerType("node-sketch-config",nodeSketchConfig);
     }
-    RED.nodes.registerType("configx",nodeSketchConfig);
-}
+
+};*/
